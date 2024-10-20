a691b5ef09ae75c3a0d9a5df3184817c0202995a
Daniel.Kahma <you@example.com>
2024-10-20 20:01:11 +0200

    Added the initial menu and added a new class for the currency

--- src/main/java/se/danielk/Main.java
+++ src/main/java/se/danielk/Main.java
@@ -1,8 +1,27 @@
 package se.danielk;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("-----Daniels Currency Converter!-----\n\n");
+        System.out.printf("Choose an option.\n\n");
+        System.out.println("1. Convert SEK to USD");
+        System.out.println("2. Convert USD to SEK");
+        System.out.println("3. Convert SEK to Euro");
+        System.out.println("4. Convert Euro to SEK");
+        System.out.printf("Answer: ");
+
+        String chooseNumber = scanner.nextLine();
+        System.out.println("\n\nEnter The amount.");
+        System.out.printf("\nAmount: ");
+
+        String chooseAmount = scanner.nextLine();
+
+
 
     }
 }
