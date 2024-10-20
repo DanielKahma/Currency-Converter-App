d290460726dee8c3d0d36efd266bdf5bea99c254
Daniel.Kahma <you@example.com>
2024-10-20 21:23:57 +0200

    Added most of the code for the converet, just trying to fix some minor issues with it

--- src/main/java/se/danielk/Currency.java
+++
@@ -1,4 +0,0 @@
-package se.danielk;
-
-public class Currency {
-}
--- src/main/java/se/danielk/Main.java
+++ src/main/java/se/danielk/Main.java
@@ -5,23 +5,85 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        System.out.printf("-----Daniels Currency Converter!-----\n\n");
-        System.out.printf("Choose an option.\n\n");
+
+
+        System.out.println("-----Daniels Currency Converter!-----\n\n");
+        System.out.println("Choose an option.\n\n");
         System.out.println("1. Convert SEK to USD");
         System.out.println("2. Convert USD to SEK");
         System.out.println("3. Convert SEK to Euro");
         System.out.println("4. Convert Euro to SEK");
-        System.out.printf("Answer: ");
+        System.out.println("Option: ");
 
-        String chooseNumber = scanner.nextLine();
-        System.out.println("\n\nEnter The amount.");
-        System.out.printf("\nAmount: ");
+        int option = scanner.nextInt();
+        String answer;
+        switch (option) {
 
-        String chooseAmount = scanner.nextLine();
+            case 1:
+                System.out.println("Enter the amount of SEK you would like to convert: ");
 
+                double SEK = scanner.nextDouble();
+
+
+                if (SEK > 0) {
+                    System.out.println(SEK + " SEK equals to " + SEK * 0.09 + " USD");
+                } else {
+                    System.out.println("Invalid Amount!");
+                }
+
+
+                break;
+            case 2:
+                System.out.println("Enter the amount of USD you would like to convert: ");
+
+                double USD = scanner.nextDouble();
+
+                if (USD > 0) {
+                    System.out.println(USD + "USD equals to " + USD * 10.52 + " SEK");
+                } else {
+                    System.out.println("Invalid Amount!");
+                }
+
+                break;
+            case 3:
+                System.out.println("Enter the amount of SEK you would like to convert: ");
+
+                double SEK2 = scanner.nextDouble();
+
+                if (SEK2 > 0) {
+                    System.out.println(SEK2 + " SEK equals to " + SEK2 * 0.08 + " EURO");
+                } else {
+                    System.out.println("Invalid Amount!");
+                }
+
+                break;
+            case 4:
+                System.out.println("Enter the amount of EURO you would like to convert: ");
+
+                double EURO = scanner.nextDouble();
+
+                if (EURO > 0) {
+                    System.out.println(EURO + " EURO equals to " + EURO * 11.43 + " SEK");
+                } else {
+                    System.out.println("Invalid Amount!");
+                }
+
+                break;
+            case 0:
+                scanner.close();
+
+                break;
+
+            default:
+                System.out.println("Invalid Answer!");
+        }
+
+
+            System.out.println("Would you like to convert some more currency?");
+            answer = scanner.next();
+        }
+        while (answer.equals("yes"));
 
 
     }
-}
